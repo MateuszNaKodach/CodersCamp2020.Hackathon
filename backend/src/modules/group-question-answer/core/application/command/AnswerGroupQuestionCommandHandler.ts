3dc3c2cf90b7241d3ccb861d2e9960b7eeb9b78f
PiotrWR <pwrynio@gmail.com>
2021-04-23 23:59:28 +0200

    Add command

---
+++ backend/src/modules/group-question-answer/core/application/command/AnswerGroupQuestionCommandHandler.ts
@@ -0,0 +1,18 @@
+import {CommandHandler} from '../../../../../shared/core/application/command/CommandHandler';
+import {AnswerGroupQuestion} from './AnswerGroupQuestion';
+import {CommandResult} from '../../../../../shared/core/application/command/CommandResult';
+import {DomainEventPublisher} from '../../../../../shared/core/application/event/DomainEventBus';
+import {CurrentTimeProvider} from '../../../../../shared/core/CurrentTimeProvider';
+import {PlayerId} from '../../../../../shared/core/domain/PlayerId';
+
+export class AnswerGroupQuestionCommandHandler implements CommandHandler<AnswerGroupQuestion> {
+  constructor(
+     private readonly eventPublisher: DomainEventPublisher,
+     private readonly currentTimeProvider: CurrentTimeProvider,
+  ) {
+  }
+
+  async execute(command: AnswerGroupQuestion): Promise<CommandResult> {
+    return CommandResult.success();
+  }
+}
