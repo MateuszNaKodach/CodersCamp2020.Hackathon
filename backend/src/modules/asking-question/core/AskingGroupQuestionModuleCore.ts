565f861468ce2e82040f6fd16ad74f4b3d6da07f
Tomek <tomasz.dworniczak@hotmail.com>
2021-04-24 01:32:00 +0200

    Rename module directory and files

--- backend/src/modules/asking-question/core/AskingGroupQuestionModuleCore.ts
+++ backend/src/modules/asking-question/core/AskingGroupQuestionModuleCore.ts
@@ -1,17 +1,24 @@
 import {DomainEventPublisher} from "../../../shared/core/application/event/DomainEventBus";
 import {CommandPublisher} from "../../../shared/core/application/command/CommandBus";
 import {CurrentTimeProvider} from "../../../shared/core/CurrentTimeProvider";
-import {PlayerProfilesRepository} from "../../player-profiles/core/application/PlayerProfilesRepository";
 import {ModuleCore} from "../../../shared/core/ModuleCore";
+import {AskGroupQuestionCommandHandler} from "./application/command/AskGroupQuestionCommandHandler";
+import {AskGroupQuestion} from "./application/command/AskGroupQuestion";
+import {AskingGroupQuestionRepository} from "./application/AskingGroupQuestionRepository";
 
 export function PlayerProfilesModuleCore(
     eventPublisher: DomainEventPublisher,
     commandPublisher: CommandPublisher,
     currentTimeProvider: CurrentTimeProvider,
-    playerProfileRepository: PlayerProfilesRepository,
+    groupQuestionRepository: AskingGroupQuestionRepository,
 ): ModuleCore {
     return {
-        commandHandlers: [],
+        commandHandlers: [
+            {
+                commandType: AskGroupQuestion,
+                handler: new AskGroupQuestionCommandHandler(eventPublisher, currentTimeProvider, groupQuestionRepository),
+            },
+        ],
         eventHandlers: [],
         queryHandlers: [],
     };
--- backend/src/modules/asking-question/core/application/command/AskGroupQuestionCommandHandler.ts
+++ backend/src/modules/asking-question/core/application/command/AskGroupQuestionCommandHandler.ts
@@ -1,29 +1,24 @@
 import {CommandHandler} from "../../../../../shared/core/application/command/CommandHandler";
-import {OpenTournamentRegistrations} from "../../../../tournaments-registrations/core/application/command/OpenTournamentRegistrations";
 import {DomainEventPublisher} from "../../../../../shared/core/application/event/DomainEventBus";
 import {CurrentTimeProvider} from "../../../../../shared/core/CurrentTimeProvider";
-import {TournamentRegistrationsRepository} from "../../../../tournaments-registrations/core/application/TournamentRegistrationsRepository";
 import {CommandResult} from "../../../../../shared/core/application/command/CommandResult";
-import {TournamentId} from "../../../../tournaments-registrations/core/domain/TournamentId";
-import {openTournamentRegistrations} from "../../../../tournaments-registrations/core/domain/TournamentRegistrations";
 import {AskGroupQuestion} from "./AskGroupQuestion";
+import {AskingGroupQuestionRepository} from "../AskingGroupQuestionRepository";
+import {askGroupQuestion} from "../../domain/GroupQuestion";
 
 export class AskGroupQuestionCommandHandler implements CommandHandler<AskGroupQuestion> {
     constructor(
         private readonly eventPublisher: DomainEventPublisher,
         private readonly currentTimeProvider: CurrentTimeProvider,
-        private readonly repository: TournamentRegistrationsRepository,
+        private readonly repository: AskingGroupQuestionRepository,
     ) {
     }
 
     async execute(command: AskGroupQuestion): Promise<CommandResult> {
-        const tournamentId = TournamentId.from(command.tournamentId);
-        const tournamentRegistrations = await this.repository.findByTournamentId(tournamentId);
-
         const {
             state,
             events
-        } = openTournamentRegistrations(tournamentRegistrations, {tournamentId}, this.currentTimeProvider);
+        } = askGroupQuestion(command, this.currentTimeProvider);
 
         await this.repository.save(state);
         this.eventPublisher.publishAll(events);
