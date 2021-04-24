45a4de5fde903dce1bacdddcf615f02339976eba
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2021-04-24 11:11:41 +0200

    Hackathon | Backend | Add google authentication (#28)
    Hackathon | Backend | Add google authentication

--- backend/src/shared/infrastructure/rest-api/Server.ts
+++ backend/src/shared/infrastructure/rest-api/Server.ts
@@ -4,12 +4,18 @@ import YAML from 'yamljs';
 import { ModuleRestApi } from '../../presentation/rest-api/ModuleRestApi';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { googleAuth } from '../../GoogleAuthentication';
+
+export type AuthorizedUser = {
+  userId: string;
+};
 
 export function restApiExpressServer(modules: ModuleRestApi[] = []): Express {
   const swaggerDocument = YAML.load('./src/rest-api-docs.yaml');
   const server = express();
   server.use(bodyParser.json());
   server.use(cors());
+  //googleAuthorizationFor(server);
 
   modules.forEach((restApiModule) => {
     server.use('/rest-api' + restApiModule.path, restApiModule.router);
@@ -27,3 +33,14 @@ export function restApiExpressServer(modules: ModuleRestApi[] = []): Express {
 
   return server;
 }
+
+// function googleAuthorizationFor(server: Express) {
+//   if (process.env.GOOGLE_CLIENT_ID !== undefined) {
+//     server.use(async (request, response, next) => {
+//       const googleTokenId: string | undefined = request.headers.authorization;
+//       const googleAuthorizedUser = await googleAuth(googleTokenId);
+//       request.authenticatedUser = googleAuthorizedUser;
+//       next();
+//     });
+//   }
+// }
