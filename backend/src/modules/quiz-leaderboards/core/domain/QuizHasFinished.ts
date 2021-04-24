dddac02fe1c5c6dba40ebe4d8c8ce4e75e13cafa
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2021-04-24 06:06:16 +0200

    Hackathon | Backend | Add quiz scores for user

---
+++ backend/src/modules/quiz-leaderboards/core/domain/QuizHasFinished.ts
@@ -0,0 +1,11 @@
+import { DomainEvent } from '../../../../shared/domain/event/DomainEvent';
+
+export class QuizHasFinished implements DomainEvent {
+  readonly occurredAt: Date;
+  readonly quizId: string;
+
+  constructor(occurredAt: Date, quizId: string) {
+    this.occurredAt = occurredAt;
+    this.quizId = quizId;
+  }
+}
