f38514549a1a2b702c0d9dcb896b3fc1595ca2e1
Tomek <tomasz.dworniczak@hotmail.com>
2021-04-24 03:35:02 +0200

    Add in memory repository

---
+++ backend/src/modules/asking-question/infrastructure/repository/inmemory/InMemoryAskingGroupQuestionRepository.ts
@@ -0,0 +1,14 @@
+import {AskingGroupQuestionRepository} from "../../../core/application/AskingGroupQuestionRepository";
+import {GroupQuestion} from "../../../core/domain/GroupQuestion";
+
+export class InMemoryAskingGroupQuestionRepository implements AskingGroupQuestionRepository {
+    private readonly entities: { [id: string]: GroupQuestion } = {};
+
+    findByGroupId(groupId: string): Promise<GroupQuestion | undefined> {
+        return Promise.resolve(this.entities[groupId]);
+    }
+
+    async save(question: GroupQuestion): Promise<void> {
+        this.entities[question.questionId] = question;
+    }
+}
