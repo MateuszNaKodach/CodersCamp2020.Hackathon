e7aee8be1ca56e7e2078e5241c914efca38be3ad
Piotr Witold Rynio <41823689+PiotrWR@users.noreply.github.com>
2021-04-24 11:17:00 +0200

    Frontend - side bar style (#34)
    * Fix height of page
    
    * style sidebar

--- frontend/src/components/atoms/alignedImages/Logo.tsx
+++ frontend/src/components/atoms/alignedImages/Logo.tsx
@@ -4,7 +4,7 @@ import { PATH_FOR_IMAGES } from '../constants/imgPaths';
 export default function Logo() {
   return (
     <div>
-      <img src={`${PATH_FOR_IMAGES}/logo.svg`} alt="logo" />
+      <img src={`${PATH_FOR_IMAGES}/logo.svg`} alt='logo' style={{ width: '100%', paddingLeft: '5%' }} />
     </div>
   );
 }
