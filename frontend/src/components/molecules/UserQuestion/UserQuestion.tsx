ce5efb26786c723e0d2034e00fe4d827511e6dd8
Pawel Szambelan <p.szambelan1@gmail.com>
2021-04-24 10:56:01 +0200

    user question view - reformat code

--- frontend/src/components/molecules/UserQuestion/UserQuestion.tsx
+++ frontend/src/components/molecules/UserQuestion/UserQuestion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ClickButton from '../../atoms/Button/ClickButton';
-import { Grid, TextField } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@material-ui/core';
 import * as yup from "yup";
 import { useFormik } from "formik";
 import EditIcon from '@material-ui/icons/Edit';
@@ -10,6 +10,11 @@ const validationSchema = yup.object({
     .string()
     .required("Pytanie jest puste..."),
 });
+
+const useStyles = makeStyles((theme) => ({
+  textField: { padding: '10px', marginBottom: '60px', backgroundColor: '#FFFFFF', width: '70%', zIndex: 1 },
+  grid: { marginTop: '150px' },
+}));
 
 export function UserQuestion() {
 
@@ -27,19 +32,19 @@ export function UserQuestion() {
     },
   });
 
+  const classes = useStyles();
   return (
-
       <form onSubmit={formik.handleSubmit}>
         <Grid
           container
           direction="column"
           justify="center"
           alignItems="center"
-          style={{marginTop: "150px"}}
+          className={classes.grid}
         >
       <TextField
         id="question"
-        style={{ padding: "10px", marginBottom: "60px", backgroundColor: '#FFFFFF', width: "70%"}}
+        className={classes.textField}
         name='question'
         placeholder="Twoje pytanie?"
         value={formik.values.question}
--- frontend/src/components/organisms/GroupQuestionContainer/GroupQuestionContainer.tsx
+++ frontend/src/components/organisms/GroupQuestionContainer/GroupQuestionContainer.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { UserQuestion } from '../../molecules/UserQuestion/UserQuestion';
-import { Container, Typography } from '@material-ui/core';
+import { Container, makeStyles } from '@material-ui/core';
 import LeftSplash from '../../atoms/alignedImages/LeftSplash';
 import AnswerIlu from '../../atoms/alignedImages/AnswerIlu';
 
+const useStyles = makeStyles((theme) => ({
+  container: {height: "100%", width: "100%", textAlign: "center"}
+}));
+
 export function GroupQuestionContainer() {
 
+  const classes = useStyles();
   return (
-    <Container style={{height: "100%", width: "100%", textAlign: "center"}}>
-      {/*<Typography variant='h3' style={{margin: "90px 0"}}>Zadane pytanie</Typography>*/}
+    <Container className={classes.container}>
       <UserQuestion />
       <AnswerIlu />
       <LeftSplash />
