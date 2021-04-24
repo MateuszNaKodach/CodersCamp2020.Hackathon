e3692bab8a395ae789ed59580ca6db94bcada9a6
Paweł <p.szambelan1@gmail.com>
2021-04-24 15:22:28 +0200

    Cuurent group question view (#49)

--- frontend/src/components/atoms/constants/routerPaths.tsx
+++ frontend/src/components/atoms/constants/routerPaths.tsx
@@ -2,3 +2,4 @@ export const PATH_FOR_MAIN_VIEW: string = "/";
 export const PATH_FOR_USER_QUESTION: string = "/question"
 export const PATH_FOR_USER_QUIZ: string = "/quiz"
 export const PATH_FOR_USER_ANSWER: string = "/answer"
+export const PATH_FOR_UNKNOWN_QUESTION: string = "/questionUnknown"
