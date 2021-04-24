c537929a7acc23e0bb3e2f54ae7ce79713c6f3b8
Anna Lamperska <anna.lamperska@nokia.com>
2021-04-24 02:51:28 +0200

    Add tests

--- backend/src/modules/questions/core/domain/event/QuestionWasDefined.ts
+++ backend/src/modules/questions/core/domain/event/QuestionWasDefined.ts
@@ -2,8 +2,16 @@ import { DomainEvent } from '../../../../../shared/domain/event/DomainEvent';
 
 export class QuestionWasDefined implements DomainEvent {
   readonly occurredAt: Date;
+  readonly questionId: string;
+  readonly groupId: string;
+  readonly text: string;
+  readonly authorId: string;
 
-  constructor(props: { occurredAt: Date }) {
+  constructor(props: { occurredAt: Date; questionId: string; groupId: string; text: string; authorId: string }) {
     this.occurredAt = props.occurredAt;
+    this.questionId = props.questionId;
+    this.groupId = props.groupId;
+    this.text = props.text;
+    this.authorId = props.authorId;
   }
 }
