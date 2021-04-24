b4ecb0d5f9ba888224aef9823f5f11a7812d3b7a
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2021-04-24 12:14:06 +0200

    Hackathon | Frontend | Cookies with hooks

--- frontend/src/components/atoms/UserShort/UserNameAndOrganization.tsx
+++ frontend/src/components/atoms/UserShort/UserNameAndOrganization.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { THEME } from '../constants/ThemeMUI';
+import {useCookie} from "react-use";
 
 export default function UserNameAndOrganization() {
+  const [currentUserCookie] = useCookie("currentUser");
+  const user = currentUserCookie ? JSON.parse(currentUserCookie) : undefined;
+
   return (
     <div>
-      <Typography style={{ fontWeight: 700, fontSize: '16px', color: `${THEME.palette.primary.main}` }}>Name
-        Surname</Typography>
+      <Typography style={{ fontWeight: 700, fontSize: '16px', color: `${THEME.palette.primary.main}` }}>{user?.email}</Typography>
       <Typography style={{
         fontWeight: 400,
         fontSize: '11px',
