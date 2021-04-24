470f6bd2811411b4e85d1b3664e05b649a8c6bbe
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-24 16:39:05 +0200

    Hackathon | Frontend | Add saving displayname while logging

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,10 @@
-import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import GoogleLogin, {GoogleLoginResponse, GoogleLoginResponseOffline} from 'react-google-login';
 import React from 'react';
-import { Grid, makeStyles, Typography } from '@material-ui/core';
+import {Grid, makeStyles, Typography} from '@material-ui/core';
 import LogInIlu from '../../atoms/alignedImages/LogInIlu';
-import {useCookie} from "react-use";
+import {useAsyncFn, useCookie} from "react-use";
 import LogInSplash from '../../atoms/alignedImages/LogInSplash';
-import {useHistory} from "react-router-dom";
+import {UserAccountsRestApi} from "../../../restapi/user-accounts/UserAccountsRestApi";
 
 const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -35,6 +35,10 @@ export function LoginPage(props: { onAuthenticated?: (user: { email: string, use
   const [currentUserCookie, updateCurrentUserCookie, deleteCurrentUserCookie] = useCookie("currentUser");
   const [currentAuthenticationToken, updateAuthenticationTokenCookie, deleteAuthenticationTokenCookie] = useCookie("authenticationToken");
 
+  const [postUserAccountDisplayNameState, postUserAccountDisplayName] = useAsyncFn((props: { userId: string, displayName: string }) =>
+      UserAccountsRestApi()
+          .postDisplayName({userId: props.userId, displayName: props.displayName})
+  )
 
   const onSuccess = (loginResponse: GoogleLoginResponse | GoogleLoginResponseOffline) => {
     console.log("GOOGLE RESPONSE", loginResponse)
@@ -44,9 +48,12 @@ export function LoginPage(props: { onAuthenticated?: (user: { email: string, use
       return;
     }
     const successResponse = loginResponse as GoogleLoginResponse;
-    updateAuthenticationTokenCookie(JSON.stringify({ value: successResponse.tokenId }))
-    if(props.onAuthenticated){
-      props.onAuthenticated({ email: successResponse.profileObj.email, userId: successResponse.profileObj.googleId, displayName: `${successResponse.profileObj.givenName} ${successResponse.profileObj.familyName}` });
+    updateAuthenticationTokenCookie(JSON.stringify({value: successResponse.tokenId}))
+    if (props.onAuthenticated) {
+      const displayName = `${successResponse.profileObj.givenName} ${successResponse.profileObj.familyName}`;
+      const userId = successResponse.profileObj.googleId;
+      props.onAuthenticated({email: successResponse.profileObj.email, userId: userId, displayName: displayName});
+      postUserAccountDisplayName({userId, displayName}).then().catch()
     }
   };
   const onFailure = (error: any) => {
@@ -59,32 +66,32 @@ export function LoginPage(props: { onAuthenticated?: (user: { email: string, use
   }
 
   return (
-    <>
-      <Grid container className={styles.mainGrid}>
-        <Grid item xs={12} md={6}></Grid>
-        <Grid item xs={12} md={6} className={styles.insideGrid}>
-          <Typography variant="h3" style={{ fontWeight: 900 }}>
-            Cześć!
-          </Typography>
-          <Typography variant="h6">Poznaj swój nowy zespół dzięki Integramic.</Typography>
-          <div style={{ marginTop: '2rem' }} />
-          <GoogleLogin
-            clientId={googleClientId}
-            buttonText="Zaloguj się za pomocą Google"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={'single_host_origin'}
-            isSignedIn={true}
-          />
+      <>
+        <Grid container className={styles.mainGrid}>
+          <Grid item xs={12} md={6}></Grid>
+          <Grid item xs={12} md={6} className={styles.insideGrid}>
+            <Typography variant="h3" style={{fontWeight: 900}}>
+              Cześć!
+            </Typography>
+            <Typography variant="h6">Poznaj swój nowy zespół dzięki Integramic.</Typography>
+            <div style={{marginTop: '2rem'}} />
+            <GoogleLogin
+                clientId={googleClientId}
+                buttonText="Zaloguj się za pomocą Google"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
+            />
+          </Grid>
+          <LogInIlu />
+          <LogInSplash />
         </Grid>
-        <LogInIlu />
-        <LogInSplash />
-      </Grid>
-      <div className={styles.bottomBar}>
-        <Typography variant="body1" style={{ color: 'white' }}>
-          CodersCamp Hackathon 2020
-        </Typography>
-      </div>
-    </>
+        <div className={styles.bottomBar}>
+          <Typography variant="body1" style={{color: 'white'}}>
+            CodersCamp Hackathon 2020
+          </Typography>
+        </div>
+      </>
   );
 }
