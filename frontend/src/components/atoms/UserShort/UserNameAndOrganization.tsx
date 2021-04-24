2a0b51b448f64e474fb6e2752216665b27345e70
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-24 12:38:41 +0200

    Hackathon | Frontend | Question on backend

--- frontend/src/components/atoms/UserShort/UserNameAndOrganization.tsx
+++ frontend/src/components/atoms/UserShort/UserNameAndOrganization.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { THEME } from '../constants/ThemeMUI';
 import {useCookie} from "react-use";
+import {GoogleLogout} from "react-google-login";
+import { useHistory } from 'react-router-dom';
 
 export default function UserNameAndOrganization() {
-  const [currentUserCookie] = useCookie("currentUser");
+  const history = useHistory();
+  const [currentUserCookie, updateCurrentUserCookie, deleteCurrentUserCookie] = useCookie("currentUser");
+  const [currentAuthenticationToken, updateAuthenticationTokenCookie, deleteAuthenticationTokenCookie] = useCookie("authenticationToken");
   const user = currentUserCookie ? JSON.parse(currentUserCookie) : undefined;
+
+  const onSuccess = () => {
+    history.push("/");
+    deleteCurrentUserCookie()
+    deleteAuthenticationTokenCookie()
+  };
 
   return (
     <div>
@@ -15,6 +25,11 @@ export default function UserNameAndOrganization() {
         fontSize: '11px',
         color: `${THEME.palette.secondary.main}`,
       }}>Organization</Typography>
+      <GoogleLogout
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
+          buttonText="Logout"
+          onLogoutSuccess={onSuccess}
+      />
     </div>
   );
 }
