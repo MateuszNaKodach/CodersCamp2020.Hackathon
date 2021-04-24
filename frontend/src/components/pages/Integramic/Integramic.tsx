5b87443723def4987d403a5757d632347628f4bf
PiotrWR <pwrynio@gmail.com>
2021-04-24 06:00:34 +0200

    Add css

--- frontend/src/components/pages/Integramic/Integramic.tsx
+++ frontend/src/components/pages/Integramic/Integramic.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  makeStyles,
   MuiThemeProvider,
   Typography,
   unstable_createMuiStrictModeTheme as createMuiTheme,
@@ -8,15 +9,29 @@ import { THEME } from '../../atoms/constants/ThemeMUI';
 import ClickButton from '../../atoms/Button/ClickButton';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {  PATH_FOR_MAIN_VIEW } from '../../atoms/constants/routerPaths';
+import clsx from 'clsx';
+import { DRAWER_WIDTH } from '../../atoms/constants/sizes';
 
 const onClick = () => {
 };
 
 export function Integramic() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   return (
     <MuiThemeProvider theme={THEME}>
 
       <Router>
+
+
 
         <Switch>
 
@@ -33,3 +48,83 @@ export function Integramic() {
     </MuiThemeProvider>
   );
 }
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: DRAWER_WIDTH,
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: DRAWER_WIDTH,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
