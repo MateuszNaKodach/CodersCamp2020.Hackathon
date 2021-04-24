ab20ac173ee4a1051bc1e8dce751a4b11b3b7e1e
Pawel Szambelan <p.szambelan1@gmail.com>
2021-04-24 06:28:21 +0200

    asking questions - test fix

--- backend/test/modules/questions/core/application/TestQuestionsModule.ts
+++ backend/test/modules/questions/core/application/TestQuestionsModule.ts
@@ -1,8 +1,10 @@
 import { InMemoryQuestionsRepository } from '../../../../../src/modules/questions/infrastructure/repository/inmemory/InMemoryQuestionsRepository';
 import { TestModuleCore, testModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
 import { QuestionsModuleCore } from '../../../../../src/modules/questions/core/QuestionsModuleCore';
+import { InMemoryGroupQuestionsRepository } from '../../../../../src/modules/questions/infrastructure/repository/inmemory/InMemoryGroupQuestionsRepository';
 
 export function testQuestionsModule(currentTime: Date): TestModuleCore {
   const questionsRepository = new InMemoryQuestionsRepository();
-  return testModuleCore((commandBus, eventBus, queryBus) => QuestionsModuleCore(eventBus, () => currentTime, questionsRepository));
+  const groupQuestionsRepository = new InMemoryGroupQuestionsRepository();
+  return testModuleCore((commandBus, eventBus, queryBus) => QuestionsModuleCore(eventBus,commandBus, () => currentTime, groupQuestionsRepository, questionsRepository));
 }
