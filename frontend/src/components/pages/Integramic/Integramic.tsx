1d44f90644731fb7af581a24482126f7e2bb62af
PiotrWR <pwrynio@gmail.com>
2021-04-24 08:06:41 +0200

    delete non-used code lines

--- frontend/src/components/pages/Integramic/Integramic.tsx
+++ frontend/src/components/pages/Integramic/Integramic.tsx
@@ -1,26 +1,12 @@
 import React, { useState } from 'react';
 import {
-  Badge,
-  CssBaseline, Divider, Drawer, List,
+  CssBaseline,
   makeStyles,
   MuiThemeProvider,
-  Typography,
-  unstable_createMuiStrictModeTheme as createMuiTheme,
 } from '@material-ui/core';
 import { THEME } from '../../atoms/constants/ThemeMUI';
-import ClickButton from '../../atoms/Button/ClickButton';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { PATH_FOR_MAIN_VIEW } from '../../atoms/constants/routerPaths';
-import clsx from 'clsx';
-import { DRAWER_WIDTH } from '../../atoms/constants/sizes';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import { AppSidebar } from '../../organisms/AppSidebar/AppSidebar';
 import { AppContext } from '../../atoms/hooks/AppContext';
-import UserAvatarAndName from '../../molecules/UserAvatarAndName/UserAvatarAndName';
 import { AppBar } from '../../organisms/AppBar/AppBar';
 import { AppMain } from '../../organisms/AppMain/AppMain';
 import { LoginPage } from '../LoginPage/LoginPage';
@@ -36,8 +22,6 @@ export function Integramic() {
   const handleDrawerClose = () => {
     setIsOpenDrawer(false);
   };
-
-  // const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   const [state, setState] = useState<AuthState>({authenticatedUser: undefined, isLoading: false});
   if (state.authenticatedUser === undefined) {
