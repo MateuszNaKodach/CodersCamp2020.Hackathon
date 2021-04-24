51f12da22391c33c433b752f758e958563e79295
Paweł <p.szambelan1@gmail.com>
2021-04-24 06:33:47 +0200

    Feature/asking question (#9)

--- backend/src/modules/questions/core/QuestionsModuleCore.ts
+++ backend/src/modules/questions/core/QuestionsModuleCore.ts
@@ -4,10 +4,20 @@ import { UserQuestionsRepository } from './application/UserQuestionsRepository';
 import { CurrentTimeProvider } from '../../../shared/core/CurrentTimeProvider';
 import { DefineQuestion } from './application/command/DefineQuestion';
 import { DefineQuestionCommandHandler } from './application/command/DefineQuestionCommandHandler';
+import { GroupQuestionWasAsked } from '../../asking-question/core/domain/event/GroupQuestionWasAsked';
+import { GroupQuestionAskedEventHandler } from './application/event/GroupQuestionAskedEventHandler';
+import { GroupQuestionsRepository } from './application/GroupQuestionsRepository';
+import { QuestionWasDefined } from './domain/event/QuestionWasDefined';
+import { QuestionWasDefinedEventHandler } from './application/event/QuestionWasDefinedEventHandler';
+import { TimeHasPassed } from '../../time/core/domain/event/TimeHasPassed';
+import { TimeHasPassedEventHandler } from './application/event/TimeHasPassedEventHandler';
+import { CommandPublisher } from '../../../shared/core/application/command/CommandBus';
 
 export function QuestionsModuleCore(
   eventPublisher: DomainEventPublisher,
+  commandPublisher: CommandPublisher,
   currentTimeProvider: CurrentTimeProvider,
+  groupQuestionsRepository: GroupQuestionsRepository,
   questionsRepository: UserQuestionsRepository,
 ): ModuleCore {
   return {
@@ -17,7 +27,20 @@ export function QuestionsModuleCore(
         handler: new DefineQuestionCommandHandler(eventPublisher, currentTimeProvider, questionsRepository),
       },
     ],
-    eventHandlers: [],
+    eventHandlers: [
+      {
+        eventType: GroupQuestionWasAsked,
+        handler: new GroupQuestionAskedEventHandler(groupQuestionsRepository),
+      },
+      {
+        eventType: QuestionWasDefined,
+        handler: new QuestionWasDefinedEventHandler(groupQuestionsRepository),
+      },
+      {
+        eventType: TimeHasPassed,
+        handler: new TimeHasPassedEventHandler(groupQuestionsRepository, commandPublisher),
+      },
+    ],
     queryHandlers: [],
   };
 }
