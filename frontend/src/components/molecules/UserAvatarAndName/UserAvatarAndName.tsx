85af8b04fbabb2c2e13325f4a293f3500c3feaa0
PiotrWR <pwrynio@gmail.com>
2021-04-24 11:12:49 +0200

    style sidebar

--- frontend/src/components/molecules/UserAvatarAndName/UserAvatarAndName.tsx
+++ frontend/src/components/molecules/UserAvatarAndName/UserAvatarAndName.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import InitialsAvatar from '../../atoms/UserShort/InitialsAvatar';
 import UserNameAndOrganization from '../../atoms/UserShort/UserNameAndOrganization';
+import { Divider } from '@material-ui/core';
 
 export default function UserAvatarAndName() {
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <InitialsAvatar />
-      <UserNameAndOrganization />
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Divider />
+        <InitialsAvatar />
+        <UserNameAndOrganization />
+      </div>
     </div>
   );
 }
