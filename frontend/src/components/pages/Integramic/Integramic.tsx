ad61f0840e812ceca1b8d99f77dad6af2cf5fd07
sabinarusielik <74836534+sabinarusielik@users.noreply.github.com>
2021-04-24 06:11:33 +0200

    Add user avatar, name and organization molecule (#17)

--- frontend/src/components/pages/Integramic/Integramic.tsx
+++ frontend/src/components/pages/Integramic/Integramic.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import {
-  MuiThemeProvider,
-  Typography,
-  unstable_createMuiStrictModeTheme as createMuiTheme,
-} from '@material-ui/core';
+import { MuiThemeProvider, Typography, unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 import { THEME } from '../../atoms/constants/ThemeMUI';
 import ClickButton from '../../atoms/Button/ClickButton';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {  PATH_FOR_MAIN_VIEW } from '../../atoms/constants/routerPaths';
+import UserAvatarAndName from '../../molecules/UserAvatarAndName/UserAvatarAndName';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PATH_FOR_MAIN_VIEW } from '../../atoms/constants/routerPaths';
 
-const onClick = () => {
-};
+const onClick = () => {};
 
 export function Integramic() {
   return (
     <MuiThemeProvider theme={THEME}>
-
       <Router>
-
         <Switch>
-
           <Route path={PATH_FOR_MAIN_VIEW} exact>
-            <Typography variant='h2'>Responsive h3</Typography>
+            <Typography variant="h2">Responsive h3</Typography>
 
             <ClickButton text={'ZADAJ PYTANIE'} onClick={() => onClick()} />
 
+            <UserAvatarAndName />
           </Route>
-
         </Switch>
-
       </Router>
     </MuiThemeProvider>
   );
