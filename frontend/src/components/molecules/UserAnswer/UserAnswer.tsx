845739135efa4df27b94d0786d369b7c24c6f4ed
Pawel Szambelan <p.szambelan1@gmail.com>
2021-04-24 16:56:02 +0200

    Group Answer View - question group answers

--- frontend/src/components/molecules/UserAnswer/UserAnswer.tsx
+++ frontend/src/components/molecules/UserAnswer/UserAnswer.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import * as yup from 'yup';
 import { Grid, makeStyles, TextField } from '@material-ui/core';
 import { useFormik } from 'formik';
 import EditIcon from '@material-ui/icons/Edit';
 import FormButton from '../../atoms/Button/FormButton';
 import Title from '../../atoms/Title/Title';
-import { useAsyncRetry } from 'react-use';
+import { useAsyncFn, useAsyncRetry } from 'react-use';
 import { QuestionsRestApi } from '../../../restapi/questions/QuestionsRestAPI';
 import { GROUP_ID } from '../UserQuestion/UserQuestion';
 import { useHistory } from 'react-router-dom';
+import { getAuthorizedUserId } from '../../../restapi/cookies';
+import { CurrentGroupQuestionUnknown } from '../../organisms/CurrentGroupQuestionUknown/CurrentGroupQuestionUnknown';
 
 const validationSchema = yup.object({
   answer: yup
     .string()
-    .required("Odpowiedź jest pusta..."),
+    .required('Odpowiedź jest pusta...'),
 });
 
 const useStyles = makeStyles((theme) => ({
@@ -21,71 +23,67 @@ const useStyles = makeStyles((theme) => ({
   grid: { marginTop: '150px' },
 }));
 
-export function UserAnswer() {
-  const history = useHistory();
+  const [postQuestionAnswerState, postQuestionAnswer] = useAsyncFn(async (props: { groupId: string, questionId: string, answerAuthorId: string, text: string }) => {
+    await QuestionsRestApi()
+      .postCurrentGroupQuestionAnswer({ groupId: GROUP_ID, questionId: props.questionId, answerAuthorId: props.answerAuthorId, text: props.text });
+  });
 
   const currentGroupQuestion = useAsyncRetry(async () =>
     await QuestionsRestApi()
-      .getCurrentGroupQuestion({groupId: GROUP_ID })
-  )
+      .getCurrentGroupQuestion({ groupId: GROUP_ID }),
+  );
 
   const formik = useFormik({
     initialValues: {
-      answer: "",
+      answer: '',
     },
     validationSchema: validationSchema,
     onSubmit: async (values, formikHelpers) => {
       try {
-        console.log(`${values.answer}`);
-        // await postQuestion({groupId: "group1", text: values.question})
-        // formikHelpers.resetForm()
+        await postQuestionAnswer({groupId: GROUP_ID, questionId: currentGroupQuestion.value!.questionId ,answerAuthorId: getAuthorizedUserId(), text: values.answer})
       } catch (error) {
         alert(error);
       }
     },
   });
 
-  function questionUnknown() {
-    history.push('/questionUnknown');
-  }
-
   const classes = useStyles();
   return (
     <>
       {currentGroupQuestion.value?.text ? <Title text={currentGroupQuestion.value.text} /> :
-        questionUnknown()
+        <CurrentGroupQuestionUnknown/>
       }
 
-    <form onSubmit={formik.handleSubmit}>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        className={classes.grid}
-      >
-        <TextField
-          id="answer"
-          className={classes.textField}
-          name='answer'
-          placeholder="Twoja odpowiedź"
-          value={formik.values.answer}
-          onChange={formik.handleChange}
-          error={formik.touched.answer && Boolean(formik.errors.answer)}
-          helperText={formik.touched.answer && formik.errors.answer}
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          InputProps={{
-            endAdornment: (
-              <EditIcon />
-            ),
-          }}
-        />
-        <FormButton text='ODPOWIEDZ NA PYTANIE' />
-      </Grid>
-    </form>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid
+          container
+          direction='column'
+          justify='center'
+          alignItems='center'
+          className={classes.grid}
+        >
+          <TextField
+            id='answer'
+            className={classes.textField}
+            name='answer'
+            placeholder='Twoja odpowiedź'
+            value={formik.values.answer}
+            onChange={formik.handleChange}
+            error={formik.touched.answer && Boolean(formik.errors.answer)}
+            helperText={formik.touched.answer && formik.errors.answer}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            InputProps={{
+              endAdornment: (
+                <EditIcon />
+              ),
+            }}
+          />
+          <FormButton text='ODPOWIEDZ NA PYTANIE' />
+        </Grid>
+      </form>
     </>
-  )
+  );
 }
