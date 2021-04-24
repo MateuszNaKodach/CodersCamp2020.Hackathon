dddac2c2199342bee82ee319d1dc4aa2e38dcfa8
sabinarusielik <74836534+sabinarusielik@users.noreply.github.com>
2021-04-24 11:14:32 +0200

    Add title component (#32)
    * Add title component
    
    * Import fix

--- frontend/src/components/organisms/AppMain/AppMain.tsx
+++ frontend/src/components/organisms/AppMain/AppMain.tsx
@@ -1,9 +1,12 @@
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { PATH_FOR_MAIN_VIEW, PATH_FOR_USER_QUESTION } from '../../atoms/constants/routerPaths';
 import { APP_BAR_HEIGHT } from '../../atoms/constants/sizes';
-import { GroupQuestionContainer } from '../GroupQuestionContainer/GroupQuestionContainer';
 
+import React from 'react';
+import Title from '../../atoms/Title/Title';
+
+import { GroupQuestionContainer } from '../GroupQuestionContainer/GroupQuestionContainer';
 
 export function AppMain() {
   const classes = useStyles();
@@ -11,25 +14,16 @@ export function AppMain() {
   return (
     <main className={classes.content}>
       <Router>
-
         <Switch>
-
           <Route path={PATH_FOR_MAIN_VIEW} exact>
-
-            <Typography variant='h2'>Responsive h3</Typography>
-
+            <Title text="Here goes question???" />
           </Route>
 
           <Route path={PATH_FOR_USER_QUESTION} exact>
-
             <GroupQuestionContainer />
-
           </Route>
-
         </Switch>
-
       </Router>
-
     </main>
   );
 }
@@ -41,6 +35,6 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: APP_BAR_HEIGHT,
     minHeight: `calc(100vh - ${APP_BAR_HEIGHT})`,
     flexGrow: 1,
-    backgroundColor: "#E8EBEE",
+    backgroundColor: '#E8EBEE',
   },
 }));
