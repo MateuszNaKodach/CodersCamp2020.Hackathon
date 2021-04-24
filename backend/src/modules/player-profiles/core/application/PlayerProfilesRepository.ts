1a92a54fc2361d8d19e98c069a6fc46d118f14b6
Anna <59768305+lamparina@users.noreply.github.com>
2021-04-24 14:40:43 +0200

    Fix scores (#48)

---
+++ backend/src/modules/player-profiles/core/application/PlayerProfilesRepository.ts
@@ -0,0 +1,9 @@
+import { PlayerProfile } from '../domain/PlayerProfile';
+
+export interface PlayerProfilesRepository {
+  save(playerProfile: PlayerProfile): Promise<void>;
+
+  findByPlayerId(playerId: string): Promise<PlayerProfile | undefined>;
+
+  findAll(): Promise<PlayerProfile[]>;
+}
