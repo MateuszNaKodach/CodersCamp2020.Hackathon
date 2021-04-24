28d3fb312acee3caa2e38d850117398d2445e402
Tomek <tomasz.dworniczak@hotmail.com>
2021-04-24 04:21:22 +0200

    Add domain object

---
+++ backend/src/modules/group-question-answer/core/application/domain/QuestionAnswer.ts
@@ -0,0 +1,13 @@
+export class QuestionAnswer {
+    readonly questionId: string;
+    readonly groupId: string;
+    readonly answerAuthorId: string;
+    readonly text: string;
+
+    constructor(props: { questionId: string; groupId: string; answerAuthorId: string; text: string }) {
+        this.questionId = props.questionId;
+        this.groupId = props.groupId;
+        this.answerAuthorId = props.answerAuthorId;
+        this.text = props.text;
+    }
+}
